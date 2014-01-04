bcf2c65cea7534004e3908a56d925c421bd56909
Selçuk ÇUKUR <xLAZx@users.noreply.github.com>
2014-01-04 15:03:51 +0200

    Game Server : Log Yapısında değişiklik

--- src/GameServer/GameServerDlg.h
+++ src/GameServer/GameServerDlg.h
@@ -459,7 +459,7 @@ public:
 
 	KOSocketMgr<CUser> m_socketMgr;
 	ClientSocketMgr<CAISocket> m_aiSocketMgr;
-	+  FILE *m_fpDeathUser;
+    FILE *m_fpDeathUser;
     FILE *m_fpDeathNpc;
     FILE *m_fpChat;
  
