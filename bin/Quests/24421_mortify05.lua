09d7dd75f9cbea7b8f11c1bdaf205199beea7372
Selçuk ÇUKUR <xLAZx@users.noreply.github.com>
2014-01-04 16:52:51 +0200

    GameServer/Lua Gerekli Lua Dosyaları

---
+++ bin/Quests/24421_mortify05.lua
@@ -0,0 +1,52 @@
+--[°íÇàÀÇ ¼ö·ÃÀÎ]µµ¶ó³ª
+-- ±×³É ´Ý±â 168
+
+-- EVENT ´Â 100¹ø ÀÌ»ó ºÎÅÍ »ç¿ë
+
+-- UID : ¼­¹ö¿¡¼­ Á¦°øÇÏ´Â À¯Àú¹øÈ£
+-- EVENT : ¼­¹ö¿¡¼­ Á¦°øÇÏ´Â Äù½ºÆ® ¹øÈ£
+-- STEP : ¼­¹ö¿¡¼­ Á¦°øÇÏ´Â Äù½ºÆ® ³»ºÎ ´Ü°è
+
+-- À§ÀÇ ¼¼°¡Áö ÆÄ¶ó¸ÞÅ¸´Â ·ç¾Æ ½ÇÇà½Ã Ç×»ó Àü¿ªº¯¼ö·Î Á¦°øµ
+
+-- Áö¿ªº¯¼ö ¼±¾ð...
+local UserClass;
+local QuestNum;
+local Ret = 0;
+
+
+if EVENT == 168 then
+	Ret = 1;
+end
+
+-- [°íÇàÀÇ ¼ö·ÃÀÎ]µµ¶ó³ª Å¬¸¯½Ã Äù½ºÆ® Ã¼Å©  
+
+local ItemA;
+local NPC = 24421;
+local savenum = 435;
+
+if EVENT == 100 then
+ItemA = HowmuchItem(UID, 910131000);  -- 4¹øÂ° ÁõÇ¥
+    if  ItemA == 0 then  -- 4¹øÂ° ÁõÇ¥ ¾øÀ»¶§
+    SelectMsg(UID, 2, savenum, 4536, NPC, 10, 168, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    else -- 4¹øÂ° ÁõÇ¥ ÀÖÀ»¶§ 
+    SelectMsg(UID, 4, savenum, 4537, NPC, 22, 101, 23, 168, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    end
+end
+
+if EVENT == 101 then
+    SelectMsg(UID, 2, savenum, 4538, NPC, 4220, 102, 4221, 103, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+end
+
+
+if EVENT == 102 then
+   ChangePosition(UID)
+   RunExchange(UID, 486);		 
+   SaveEvent(UID, 4287);
+end
+
+if EVENT == 103 then
+    ChangePosition(UID)
+    RobItem(UID, 910131000, 1);
+    SelectMsg(UID, 2, savenum, 4539, NPC, 10, 168, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+end
